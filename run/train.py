d957ab2cfd9e184c796a04e26dc9dc228dededde
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2019-09-24 23:23:27 +0800

    train, v1

--- run/train.py
+++ run/train.py
@@ -196,7 +196,7 @@ def main():
                 if args.regularize_z:
                     kld += cf.gaussian_kl_divergence(mean, ln_var)
                 print(zi.shape)
-                factor_z.append(zi.reshape(zi[0], -1))
+                factor_z.append(zi.data.reshape(zi[0], -1))
             factor_z = xp.concatenate(factor_z)
             negative_log_likelihood += cf.gaussian_nll(factor_z, 0, 0)
             loss = (negative_log_likelihood + kld) / args.batch_size - logdet
