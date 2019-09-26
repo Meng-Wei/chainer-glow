fc3042d62bef34af598c9189dac6642b2d1a9496
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2019-09-26 14:10:32 +0800

    train loss

--- run/loss.py
+++ run/loss.py
@@ -230,11 +230,11 @@ def main():
         
 
         if iteration % 100 == 99:
-            np.save('10logs/'+str(j)+'z.npy', z_s)
-            np.save('10logs/'+str(j)+'b.npy', b_s)
-            np.save('10logs/'+str(j)+'loss.npy', loss_s)
-            np.save('10logs/'+str(j)+'logpZ.npy', logpZ_s)
-            np.save('10logs/'+str(j)+'logDet.npy', logDet_s)
+            np.save('logs/'+str(j)+'z.npy', z_s)
+            np.save('logs/'+str(j)+'b.npy', b_s)
+            np.save('logs/'+str(j)+'loss.npy', loss_s)
+            np.save('logs/'+str(j)+'logpZ.npy', logpZ_s)
+            np.save('logs/'+str(j)+'logDet.npy', logDet_s)
             z_s = []
             b_s = []
             loss_s = []
@@ -248,7 +248,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--snapshot-path", "-snapshot", type=str, default='/home/data1/meng/chainer/snapshot_64')
     parser.add_argument("--gpu-device", "-gpu", type=int, default=1)
-    parser.add_argument('--ckpt', type=str, default='10logs')
+    parser.add_argument('--ckpt', type=str, default='logs')
     # parser.add_argument("--dataset-path", "-dataset", type=str, required=False)
     # parser.add_argument("--dataset-format", "-ext", type=str, required=True)
     parser.add_argument("--total-iteration", "-iter", type=int, default=10)
