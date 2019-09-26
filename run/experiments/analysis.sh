7576c7f16c394ebe32b2fb18637445ab78481901
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2019-09-26 23:57:23 +0800

    clean up

--- run/experiments/analysis.sh
+++ run/experiments/analysis.sh
@@ -1,1 +1,1 @@
-python3 inference.py --snapshot-path /home/data1/meng/chainer/snapshot_64 -dataset /home/data1/meng/chainer/celeba-64x64-images-npy/ --gpu-device 1 -ext npy
+python3 inference.py --snapshot-path ../s1 -dataset /home/data1/meng/chainer/celeba-128x128-images-npy/ --gpu-device 1 -ext npy
--- run/experiments/inference.py
+++ run/experiments/inference.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import math
 
 import chainer
 import chainer.functions as cf
@@ -59,31 +60,34 @@ def main():
     hyperparams.print()
 
     num_bins_x = 2.0**hyperparams.num_bits_x
+    num_pixels = 3 * hyperparams.image_size[0] * hyperparams.image_size[1]
+    
+    # Get Dataset:
+    if True:
+        assert args.dataset_format in ["png", "npy"]
 
-    assert args.dataset_format in ["png", "npy"]
-
-    files = Path(args.dataset_path).glob("*.{}".format(args.dataset_format))
-    if args.dataset_format == "png":
-        images = []
-        for filepath in files:
-            image = np.array(Image.open(filepath)).astype("float32")
-            image = preprocess(image, hyperparams.num_bits_x)
-            images.append(image)
-        assert len(images) > 0
-        images = np.asanyarray(images)
-    elif args.dataset_format == "npy":
-        images = []
-        for filepath in files:
-            array = np.load(filepath).astype("float32")
-            array = preprocess(array, hyperparams.num_bits_x)
-            images.append(array)
-        assert len(images) > 0
-        num_files = len(images)
-        images = np.asanyarray(images)
-        images = images.reshape((num_files * images.shape[1], ) +
-                                images.shape[2:])
-    else:
-        raise NotImplementedError
+        files = Path(args.dataset_path).glob("*.{}".format(args.dataset_format))
+        if args.dataset_format == "png":
+            images = []
+            for filepath in files:
+                image = np.array(Image.open(filepath)).astype("float32")
+                image = preprocess(image, hyperparams.num_bits_x)
+                images.append(image)
+            assert len(images) > 0
+            images = np.asanyarray(images)
+        elif args.dataset_format == "npy":
+            images = []
+            for filepath in files:
+                array = np.load(filepath).astype("float32")
+                array = preprocess(array, hyperparams.num_bits_x)
+                images.append(array)
+            assert len(images) > 0
+            num_files = len(images)
+            images = np.asanyarray(images)
+            images = images.reshape((num_files * images.shape[1], ) +
+                                    images.shape[2:])
+        else:
+            raise NotImplementedError
 
     dataset = glow.dataset.Dataset(images)
     iterator = glow.dataset.Iterator(dataset, batch_size=1)
@@ -94,61 +98,26 @@ def main():
     if using_gpu:
         encoder.to_gpu()
 
-
-    # ori_x = []
-    # fw_logdet = []
-    # enc_z = []
-    # logpZ = []
-    # logpZ2 = []
-    # rev_x = []
-    # bk_logdet = []
-
-    pro_ori_x = []
-    pro_enc_z = []
-    pro_rev_x = []
-    pro_fw_logdet = []
-    pro_bk_logdet = []
-    pro_logpZ = []
-    pro_logpZ2 = []
-
+    ori_x = []
+    enc_z = []
+    rev_x = []
+    fw_logdet = []
+    logpZ = []
+    logpZ2 = []
+    i = 0
 
     with chainer.no_backprop_mode() and encoder.reverse() as decoder:
-        i = 0
-        j = 0
         for data_indices in iterator:
             i += 1
+
             x = to_gpu(dataset[data_indices]) # 1x3x64x64
-
-            # x_img = make_uint8(x[0], num_bins_x)
-            # ori_x.append(x_img) # 64x64x3
-            # factorized_z_distribution, fw_ldt = encoder.forward_step(x)
-            # fw_logdet.append(cupy.asnumpy(fw_ldt.data))
-
-            # factor_z = []
-            # ez = []
-            # nll = 0
-            # for (zi, mean, ln_var) in factorized_z_distribution:
-            #     nll += cf.gaussian_nll(zi, mean, ln_var)
-            #     factor_z.append(zi.data)
-            #     ez.append(zi.data.reshape(-1,))
-            
-            # ez = np.concatenate(ez)
-            # enc_z.append(ez.get())
-            # logpZ.append(cupy.asnumpy(nll.data))
-            # logpZ2.append(cupy.asnumpy(
-            #     cf.gaussian_nll(ez, np.mean(ez), np.log(np.var(ez))).data ))
-
-            # rx, bk_ldt = decoder.reverse_step(factor_z)
-            # rx_img = make_uint8(rx.data[0], num_bins_x)
-            # rev_x.append(rx_img)
-            # bk_logdet.append(cupy.asnumpy(bk_ldt.data))
-
-            # Pre-process
             x += xp.random.uniform(0, 1.0 / num_bins_x, size=x.shape)
             x_img = make_uint8(x[0], num_bins_x)
-            pro_ori_x.append(x_img) # 64x64x3
+            ori_x.append(x_img)
+
             factorized_z_distribution, fw_ldt = encoder.forward_step(x)
-            pro_fw_logdet.append(cupy.asnumpy(fw_ldt.data))
+            fw_ldt -= math.log(num_bins_x) * num_pixels
+            fw_logdet.append(cupy.asnumpy(fw_ldt.data))
 
             factor_z = []
             ez = []
@@ -159,59 +128,34 @@ def main():
                 ez.append(zi.data.reshape(-1,))
             
             ez = np.concatenate(ez)
-            pro_enc_z.append(ez.get())
-            pro_logpZ.append(cupy.asnumpy(nll.data))
-            pro_logpZ2.append(
-                cupy.asnumpy(cf.gaussian_nll(ez, np.mean(ez), np.log(np.var(ez))).data ))
+            enc_z.append(ez.get())
+            logpZ.append(cupy.asnumpy(nll.data))
+            logpZ2.append(
+                cupy.asnumpy(cf.gaussian_nll(ez, np.mean(ez), np.log(np.var(ez))).data )
+            )
 
-            rx, bk_ldt = decoder.reverse_step(factor_z)
+            rx, _ = decoder.reverse_step(factor_z)
             rx_img = make_uint8(rx.data[0], num_bins_x)
-            pro_rev_x.append(rx_img)
-            pro_bk_logdet.append(cupy.asnumpy(bk_ldt.data))
+            rev_x.append(rx_img)
 
             if i % 100 == 0:
-                j += 1
-                # np.save(str(i)+'/ori_x.npy', ori_x)
-                # fw_logdet = cupy.asnumpy(fw_logdet)
-                # np.save(str(i)+'/fw_logdet.npy', fw_logdet)
-                # np.save(str(i)+'/enc_z.npy', enc_z)
-                # logpZ = np.array(logpZ)
-                # np.save(str(i)+'/logpZ.npy', logpZ)
-                # logpZ2 = np.array(logpZ2)
-                # np.save(str(i)+'/logpZ2.npy', logpZ2)
-                # np.save(str(i)+'/rev_x.npy', rev_x)
-                # bk_logdet = np.array(bk_logdet)
-                # np.save(str(i)+'/bk_logdet.npy', bk_logdet)
+                np.save(str(i)+'/ori_x.npy', ori_x)
+                fw_logdet = np.array(fw_logdet)
+                np.save(str(i)+'/fw_logdet.npy', fw_logdet)
+                np.save(str(i)+'/enc_z.npy', enc_z)
+                logpZ = np.array(logpZ)
+                np.save(str(i)+'/logpZ.npy', logpZ)
+                logpZ2 = np.array(logpZ2)
+                np.save(str(i)+'/logpZ2.npy', logpZ2)
+                np.save(str(i)+'/rev_x.npy', rev_x)
 
-                np.save(str(i)+'/pro_ori_x.npy', pro_ori_x)
-                pro_fw_logdet = np.array(pro_fw_logdet)
-                np.save(str(i)+'/pro_fw_logdet.npy', pro_fw_logdet)
-                np.save(str(i)+'/pro_enc_z.npy', pro_enc_z)
-                pro_logpZ = np.array(pro_logpZ)
-                np.save(str(i)+'/pro_logpZ.npy', pro_logpZ)
-                pro_logpZ2 = np.array(pro_logpZ2)
-                np.save(str(i)+'/pro_logpZ2.npy', pro_logpZ2)
-                np.save(str(i)+'/pro_rev_x.npy', pro_rev_x)
-                pro_bk_logdet = np.array(pro_bk_logdet)
-                np.save(str(i)+'/pro_bk_logdet.npy', pro_bk_logdet)
-
-                # ori_x = []
-                # fw_logdet = []
-                # enc_z = []
-                # logpZ = []
-                # logpZ2 = []
-                # rev_x = []
-                # bk_logdet = []
-
-                pro_ori_x = []
-                pro_enc_z = []
-                pro_rev_x = []
-                pro_fw_logdet = []
-                pro_bk_logdet = []
-                pro_logpZ = []
-                pro_logpZ2 = []
-                if j == 1:
-                    return
+                ori_x = []
+                enc_z = []
+                rev_x = []
+                fw_logdet = []
+                logpZ = []
+                logpZ2 = []
+                return
             
 
 if __name__ == "__main__":
@@ -219,7 +163,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--snapshot-path", "-snapshot", type=str, required=True)
     parser.add_argument("--dataset-path", "-dataset", type=str, required=True)
-    parser.add_argument("--gpu-device", "-gpu", type=int, default=0)
+    parser.add_argument("--gpu-device", "-gpu", type=int, default=1)
     parser.add_argument("--dataset-format", "-ext", type=str, required=True)
     args = parser.parse_args()
     main()
