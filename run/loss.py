c40b4322884ccded9891fbb8725ddebfa324a744
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2019-09-26 14:12:55 +0800

    train loss

--- run/loss.py
+++ run/loss.py
@@ -196,8 +196,8 @@ def main():
             ez.append(zi.data.reshape(-1,))
             
         ez = np.concatenate(ez)
-        # logpZ = cf.gaussian_nll(ez, xp.zeros(ez.shape), xp.zeros(ez.shape)).data
-        logpZ = cf.gaussian_nll(ez, np.mean(ez), np.log(np.var(ez))).data
+        logpZ = cf.gaussian_nll(ez, xp.zeros(ez.shape), xp.zeros(ez.shape)).data
+        # logpZ = cf.gaussian_nll(ez, np.mean(ez), np.log(np.var(ez))).data
 
         loss = b_l2norm[0] + (logpZ - fw_ldt)
 
@@ -230,11 +230,11 @@ def main():
         
 
         if iteration % 100 == 99:
-            np.save('logs/'+str(j)+'z.npy', z_s)
-            np.save('logs/'+str(j)+'b.npy', b_s)
-            np.save('logs/'+str(j)+'loss.npy', loss_s)
-            np.save('logs/'+str(j)+'logpZ.npy', logpZ_s)
-            np.save('logs/'+str(j)+'logDet.npy', logDet_s)
+            np.save('normallogs/'+str(j)+'z.npy', z_s)
+            np.save('normallogs/'+str(j)+'b.npy', b_s)
+            np.save('normallogs/'+str(j)+'loss.npy', loss_s)
+            np.save('normallogs/'+str(j)+'logpZ.npy', logpZ_s)
+            np.save('normallogs/'+str(j)+'logDet.npy', logDet_s)
             z_s = []
             b_s = []
             loss_s = []
@@ -248,7 +248,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--snapshot-path", "-snapshot", type=str, default='/home/data1/meng/chainer/snapshot_64')
     parser.add_argument("--gpu-device", "-gpu", type=int, default=1)
-    parser.add_argument('--ckpt', type=str, default='logs')
+    parser.add_argument('--ckpt', type=str, default='normallogs')
     # parser.add_argument("--dataset-path", "-dataset", type=str, required=False)
     # parser.add_argument("--dataset-format", "-ext", type=str, required=True)
     parser.add_argument("--total-iteration", "-iter", type=int, default=10)
