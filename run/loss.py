db6174de1e191b99fe691eae324ad4080cc8d823
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2019-09-26 11:59:56 +0800

    train loss

--- run/loss.py
+++ run/loss.py
@@ -184,7 +184,7 @@ def main():
             ez.append(zi.data.reshape(-1,))
             
         ez = np.concatenate(ez)
-        logpZ = cupy.asnumpy(cf.gaussian_nll(ez, np.mean(ez), np.log(np.var(ez))).data)
+        logpZ = cf.gaussian_nll(ez, np.mean(ez), np.log(np.var(ez))).data
 
         loss = b_l2norm[0] + (logpZ - fw_ldt)
 
