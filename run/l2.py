9cf38f68d5f9fb32d14403cf8aa2ae3a092c39c8
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2019-09-28 18:46:45 +0800

    mask

--- run/l2.py
+++ run/l2.py
@@ -155,7 +155,7 @@ def main():
 
             with self.init_scope():
                 self.b = chainer.Parameter(initializers.Normal(), shape)
-                self.m = chainer.Parameter(initializers.Uniform(), (shape[1]/8, shape[1]/8))
+                self.m = chainer.Parameter(initializers.Uniform(), (16, 16))
         
         def forward(self, x):
             b = cf.tanh(self.b)
