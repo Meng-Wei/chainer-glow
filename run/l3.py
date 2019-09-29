2a053b455f6329d01e9a5ec52c373e4e8394fef5
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2019-09-29 10:12:33 +0800

    preprocess

--- run/l3.py
+++ run/l3.py
@@ -189,7 +189,7 @@ def main():
 
         logpZ = (logpZ2 + logpZ1)/2
         # loss =  1000* b_norm + logpZ * 0.5 - fw_ldt
-        loss = b_norm + (logpZ - fw_ldt)
+        loss = b_norm - (logpZ + fw_ldt) ** 2
 
         loss.backward()
         optimizer.update()
